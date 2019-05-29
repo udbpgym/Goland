6742c6f3f54187ff54e7e55b3bd32655cfcdc991
zhangzhao <471069223@qq.com>
2019-05-29 16:51:45 +0800

    再次测试一下

--- src/day02/demo5.go
+++ src/day02/demo5.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a, b, c := 10, 20, 0
+	fmt.Printf("main函数a %d\n", a)
+	fmt.Printf("main函数b %d\n", b)
+	fmt.Printf("main函数c %d\n", c)
 }
